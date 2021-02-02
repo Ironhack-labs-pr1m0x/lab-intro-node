fb13ab3cfc55326b0df820f9ad9419a11db3c940
pr1m0x <kraemer.timo1@gmail.com>
2021-02-02 14:23:49 +0100

    fin

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos !== this.items.length - 1) throw new Error('OutOfBounds');
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) throw new Error('EmptySortedList');
+    return this.items[this.items.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) throw new Error('EmptySortedList');
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) return 0;
+    return this.items.reduce((total, num) => total + num);
+  }
 
-  avg() {}
+  avg() {
+    return this.items.reduce((total, num) => total + num) / this.items.length;
+  }
 }
 
 module.exports = SortedList;
